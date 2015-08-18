d0c2dd43538175b395c95930f9c3ebc028d11cf7
vagrant <vagrant@precise32.(none)>
2015-08-18 17:12:50 +0000

    init

---
+++ gravitar3d-page.tpl.php
@@ -0,0 +1,26 @@
+
+<div id="controls">
+			
+			<div style="width:280px;" id="controls-container">
+			</div>
+			<div id="fps-container">
+			</div>
+		</div>
+		<div id="container">
+			<div id="stage">
+				<div id="overlay">
+					<h1>Drop Image Here</h1>
+				</div>
+          </div>
+</div>
+
+<div id="container">
+  <div id="stage">
+    <div id="overlay"></div>
+  </div>
+</div>
+
+<script>
+var imgPath = '<?php echo $gravitar; ?>';
+
+</script>
